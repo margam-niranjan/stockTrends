d377755f17c93a10b64f1def198e6cda3f8ea2b0
margam-niranjan <niranjanchintuwin2004@gmail.com>
2024-12-21 19:39:30 +0530

    edited corporate actions

---
+++ src/main/java/com/stocks/ApiResponse/CorporateActions/splits/SplitsData.java
@@ -0,0 +1,10 @@
+package com.stocks.ApiResponse.CorporateActions.splits;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class RightsData {
+    @JsonProperty("title")
+    private String title;
+    @JsonProperty("msg")
+    private String msg;
+}
