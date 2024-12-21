eb49d860e232cda416d546cb8b49faccd29d52b3
margam-niranjan <niranjanchintuwin2004@gmail.com>
2024-12-22 00:29:41 +0530

    updated

--- src/main/java/com/stocks/ApiResponse/stockInfoByName/StockInfoData.java
+++ src/main/java/com/stocks/ApiResponse/stockInfoByName/StockInfoData.java
@@ -1,4 +1,5 @@
 package com.stocks.ApiResponse.stockInfoByName;
 
 public class StockInfoData {
+
 }
--- src/main/java/com/stocks/service/StockService.java
+++ src/main/java/com/stocks/service/StockService.java
@@ -1,7 +1,14 @@
 package com.stocks.service;
 
+import com.stocks.ApiResponse.CorporateActions.CorporateActionsData;
+import com.stocks.ApiResponse.HeaderHttp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -10,23 +17,25 @@ import java.net.http.HttpResponse;
 
 @Service
 public class StockService {
-    HttpResponse<String> response;
+    private String industry;
+    public void setIndustry(String industry){
+        this.industry = industry;
+    }
     @Value("${stock.api.key}")
     private String apiKey;
 
     @Value("${stock.api.host}")
     private String apiHost;
-
-    public String fetchStockData(String stockName) throws Exception {
-        HttpRequest request = HttpRequest.newBuilder()
-//        https://indian-stock-exchange-api2.p.rapidapi.com/stock?name=tata%20steel
-                .uri(URI.create("https://" + apiHost + "/stock?name=" + stockName))
-                .header("x-rapidapi-key", apiKey)
-                .header("x-rapidapi-host", apiHost)
-                .method("GET", HttpRequest.BodyPublishers.noBody())
-                .build();
-        response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        return response.body();
+    private final String url="https://indian-stock-exchange-api2.p.rapidapi.com/stock?name=";
+    @Autowired
+    private RestTemplate restTemplate;
+    StringBuilder stringBuilder = new StringBuilder(url);
+    HeaderHttp header = new HeaderHttp();
+    public CorporateActionsData getCorporateActions(){
+        HttpEntity<Void> httpEntity = new HttpEntity<>(header.getHttpHeader(apiKey,apiHost));
+        String url = stringBuilder.append(industry).toString();
+        ResponseEntity<CorporateActionsData> response = restTemplate.exchange(url, HttpMethod.GET, httpEntity, CorporateActionsData.class);
+        return response.getBody();
     }
 
 }
