8602608e589bce992e8379a13ff6bdbf89fb122e
margam-niranjan <niranjan.margam.14@gmail.com>
2025-01-22 20:32:00 +0530

    added some more generalized ResponseEntity

--- src/main/java/com/stocks/controller/NseMostActiveController.java
+++ src/main/java/com/stocks/controller/NseMostActiveController.java
@@ -1,8 +1,10 @@
 package com.stocks.controller;
 
+import com.stocks.ApiResponse.GenericApiResponse;
 import com.stocks.ApiResponse.nseMostActive.NseMostActiveData;
 import com.stocks.service.NseMostActiveService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +17,16 @@ public class NseMostActiveController {
     @Autowired
     NseMostActiveService nseMostActiveService;
     @GetMapping
-    public List<NseMostActiveData> getNseActive()throws Exception{
-        return nseMostActiveService.getNseActive();
+    public ResponseEntity<GenericApiResponse<Object>> getNseActive()throws Exception{
+        try{
+            List<NseMostActiveData> response = nseMostActiveService.getNseActive();
+            return ResponseEntity.ok(GenericApiResponse.success(response));
+        }
+        catch (IllegalArgumentException ex) {
+            return ResponseEntity.badRequest().body(GenericApiResponse.error("Invalid stats type: " + ex.getMessage()));
+
+        } catch (Exception ex) {
+            return ResponseEntity.status(500).body(GenericApiResponse.error("An error occurred: " + ex.getMessage()));
+        }
     }
 }
